7a2acd35b8fe00a4b8dce4b2c023e9f80aae5882
selbowgreaser <selbowgreaser@gmail.com>
2022-03-18 17:10:26 +0300

    Исправлено форматирование и документация

--- solver_core/one_dim_opt/parabola.py
+++ solver_core/one_dim_opt/parabola.py
@@ -1,6 +1,7 @@
-from typing import Optional, Callable
+from typing import Optional, Callable, Any
 import numpy as np
 import pandas as pd
+
 
 class Parabola:
     """
@@ -8,7 +9,7 @@ class Parabola:
 
     Parameters
     ----------
-    func : Callble
+    func : Callable
         Функция, у которой надо искать минимум.
     interval_x : tuple
         Кортеж с двумя значениями типа float, которые задают ограничения для отрезка.
@@ -22,11 +23,15 @@ class Parabola:
         строку с ответом.
     save_iters_df: Optional[bool] = False
         Флаг, нужно ли сохранять информацию об итерациях в pandas.DataFrame
-
     """
 
-    def __init__(self, func: Callable, interval_x: tuple, acc: Optional[float] = 10**-5, max_iteration: Optional[int] = 500,
-                 print_interim: Optional[bool] = False, save_iters_df: Optional[bool] = False):
+    def __init__(self,
+                 func: Callable,
+                 interval_x: tuple,
+                 acc: Optional[float] = 10 ** -5,
+                 max_iteration: Optional[int] = 500,
+                 print_interim: Optional[bool] = False,
+                 save_iters_df: Optional[bool] = False):
         self.func = func
         self.interval_x = interval_x
         self.acc = acc
@@ -34,18 +39,17 @@ class Parabola:
         self.print_interm = print_interim
         self.save_iters_df = save_iters_df
 
-    def solve(self):
+    def solve(self) -> str:
         """
         Метод решает задачу.
 
         Returns
         -------
         ans: str
-            Строка с ответом и причиной остановки. Содержит информацию об итерациях при \
-            print_interm=True
+            Строка с ответом и причиной остановки. Содержит информацию об итерациях при print_interm=True
         """
 
-        x2 = (self.interval_x[1] - self.interval_x[0])*np.random.random() + self.interval_x[0]
+        x2 = (self.interval_x[1] - self.interval_x[0]) * np.random.random() + self.interval_x[0]
         self.x = [self.interval_x[0], x2, self.interval_x[1]]
         self.y = [self.func(i) for i in self.x]
         answer = ''
@@ -56,7 +60,7 @@ class Parabola:
             self.x_ = self.count_new_x()
             self.y_ = self.func(self.x_)
             if self.print_interm:
-                answer += f"iter: {i+1:<4d} x: {self.x_:.12f} y: {self.y_:.12f}\n"
+                answer += f"iter: {i + 1:<4d} x: {self.x_:.12f} y: {self.y_:.12f}\n"
             if self.save_iters_df:
                 iterations_df = iterations_df.append({'x': self.x_, 'y': self.y_}, ignore_index=True)
             if i == 0:
@@ -68,20 +72,21 @@ class Parabola:
                 story = self.x_
 
             intervals = self.new_interval()
-            if intervals == False:
+            if not intervals:
                 return 'Похоже, на отрезке нет минимума'
             self.x = intervals[0]
             self.y = intervals[1]
         else:
             answer = answer + f"Достигнуто максимальное число итераций. \nПолученная точка: {(self.x_, self.y_)}"
         return answer
-    def count_new_x(self):
+
+    def count_new_x(self) -> float:
         """
         Метод вычисляет точкку минимума у параболы при заданных трёх точках.
 
         Returns
         -------
-        x: float
+        float
             Вычисленное по формуле значение.
         """
 
@@ -91,15 +96,16 @@ class Parabola:
         x = 1 / 2 * (self.x[0] + self.x[1] - a1 / a2)
         return x
 
-    def new_interval(self):
+    def new_interval(self) -> tuple[list[float | Any], list[Any]] | bool:
         """
         Метод определяет новые точки для итерации.
 
         Returns
         -------
-        x, y: list
+        tuple[list[float | Any], list[Any]] | bool
             Значения новых точек и значения функции в них.
         """
+
         if self.x[0] <= self.x_ <= self.x[1]:
             if self.y_ > self.y[1]:
                 return [self.x_, self.x[1], self.x[2]], [self.y_, self.y[1], self.y[2]]
@@ -114,7 +120,7 @@ class Parabola:
 
 
 if __name__ == "__main__":
-    f = lambda x: -5*x**5 + 4*x**4 - 12*x**3 + 11*x**2 - 2*x + 1 # -0.5 0.5
+    f = lambda x: -5 * x**5 + 4 * x**4 - 12 * x**3 + 11 * x**2 - 2 * x + 1  # -0.5 0.5
     task = Parabola(f, (-0.5, 0.5), print_interim=True, save_iters_df=True)
     res = task.solve()
     print(res)
