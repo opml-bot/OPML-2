f950e7f870d6a2e935337f4b10fa58a86b091ab5
selbowgreaser <selbowgreaser@gmail.com>
2022-03-20 15:20:01 +0300

    Добавлены методы для регистрации пользователя в таблицу onedimopt и извлечения шага, на котором находится пользователь

--- vk_bot/answerer/one_dim_opt/onedimopt.py
+++ vk_bot/answerer/one_dim_opt/onedimopt.py
@@ -7,4 +7,23 @@ class OneDimOpt:
         self.db = db
         self.user_id = user_id
 
+    def get_step(self) -> str:
+        """
+        Извлечение шага, на котором находится пользователь при решении задачи.
 
+        Returns
+        -------
+        str
+            Шаг, на котором находится пользователь, для решения задачи.
+        """
+
+        if not self.db.select(Select.ONEDIMOPT_STEP, (self.user_id,)):
+            self.registration()
+        return self.db.select(Select.ONEDIMOPT_STEP, (self.user_id,))[0]
+
+    def registration(self):
+        """
+        Регистрация пользователя в базе данных в таблице onedimopt.
+        """
+
+        self.db.insert(Insert.ONEDIMOPT, (self.user_id,))
