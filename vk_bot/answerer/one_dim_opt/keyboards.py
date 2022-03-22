8c8528c584ccf71693c9d26c58a62bd39b3ba6c1
selbowgreaser <selbowgreaser@gmail.com>
2022-03-22 13:48:50 +0300

    Добавлена клавиатура для выбора алгоритма.

--- vk_bot/answerer/one_dim_opt/keyboards.py
+++ vk_bot/answerer/one_dim_opt/keyboards.py
@@ -5,5 +5,21 @@ class Keyboards:
     """
     Набор готовых клавиатур.
     """
+
     def __init__(self):
         self.keyboard = VkKeyboard(inline=True)
+
+    def for_opt_alg_selection(self) -> VkKeyboard:
+        """
+        Выбор алгоритма для оптимизации.
+
+        Returns
+        -------
+        VkKeyboard
+            Объект созданной клавиатуры.
+        """
+
+        self.keyboard.add_button('Алгоритм "Парабола"', VkKeyboardColor.PRIMARY)
+        self.keyboard.add_button('Алгоритм "Золотое сечение"', VkKeyboardColor.SECONDARY)
+        # TODO: добавить кнопки для остальных алгоритмов
+        return self.keyboard
