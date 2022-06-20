3ceb5d35737298a0ee03ce7d9d4ada88f33dfd96
aafrolov <TgkKm7xG>
2022-06-20 20:53:14 +0300

    добавлена визуализация для метода золотого сечения

--- solver_core/one_dim_opt/golden_ratio.py
+++ solver_core/one_dim_opt/golden_ratio.py
@@ -1,7 +1,9 @@
 from typing import Optional, Callable, Tuple
 from numbers import Real, Integral
-
+import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 class GoldenRatio:
@@ -32,13 +34,15 @@ class GoldenRatio:
                  acc: Optional[Real] = 10 ** -5,
                  max_iteration: Optional[Integral] = 500,
                  print_interim: Optional[bool] = False,
-                 save_iters_df: Optional[bool] = False):
+                 save_iters_df: Optional[bool] = False,
+                 draw_flag: Optional[bool] = False):
         self.func = func
         self.interval_x = interval_x
         self.acc = acc
         self.max_iteration = max_iteration
         self.print_interm = print_interim
         self.save_iters_df = save_iters_df
+        self.draw_flag = draw_flag
 
     def solve(self) -> str:
         """
@@ -59,6 +63,10 @@ class GoldenRatio:
         b: Real = self.interval_x[1]
         if self.save_iters_df:
             iters_df = pd.DataFrame(columns=['x', 'y'])
+        if self.draw_flag:
+            draw_df = pd.DataFrame(columns=['x', 'y', 'iter', 'size', 'color'])
+            draw_df['iter'] = draw_df['iter'].astype(int)
+            draw_df['size'] = draw_df['size'].astype(float)
         answer = ''
 
         for i in range(1, self.max_iteration):
@@ -71,6 +79,13 @@ class GoldenRatio:
                 b = x2
             self.x_ = (a + b) / 2
             self.y_ = self.func(self.x_)
+            if self.draw_flag:
+                d = {'x': [a, self.x_, b],
+                                          'y': [self.func(a), self.y_, self.func(b)],
+                                          'iter': [i]*3,
+                                          'size': [10.0]*3,
+                                          'color': ['red', 'blue', 'green']}
+                draw_df = pd.concat([draw_df, pd.DataFrame(d)])
             if self.print_interm:
                 answer += f"iter: {i + 1} x: {self.x_:.12f} y: {self.y_:.12f}\n"
             if self.save_iters_df:
@@ -80,7 +95,14 @@ class GoldenRatio:
             if abs(x1 - x2) < self.acc:
                 answer += f"Достигнута заданная точность. \nПолученная точка: {(self.x_, self.y_)}"
                 if self.save_iters_df:
-                    return answer, iters_df
+                    if self.draw_flag:
+                        a = self.draw(draw_df)
+                        return answer, iters_df, a
+                    else:
+                        return answer, iters_df
+                elif self.draw_flag:
+                    a = self.draw(draw_df)
+                    return answer, a
                 return answer
         else:
             answer = answer + f"Достигнуто максимальное число итераций. \nПолученная точка: {(self.x_, self.y_)}"
@@ -88,14 +110,22 @@ class GoldenRatio:
                 return answer, iters_df
             return answer
 
-    def draw(self):
-        """
-        Метод визуализирует работу алгоритма.
-        """
+    def draw(self, df):
+        fig = px.scatter(df, x='x', y='y', animation_frame='iter',
+                         range_x=[min(df['x']) - (max(df['x']) - min(df['x'])) * 0.15,
+                                  max(df['x']) + (max(df['x']) - min(df['x'])) * 0.15],
+                        range_y=[min(df['y']) - (max(df['y']) - min(df['y'])) * 0.15,
+                                  max(df['y']) + (max(df['y']) - min(df['y'])) * 0.15],
+                         size='size', color='color')
+        func_x = np.linspace(self.interval_x[0], self.interval_x[1], 1000)
+        func_y = [self.func(i) for i in func_x]
+        fig.add_trace(go.Scatter(x=func_x, y=func_y))
+        fig.update_layout(showlegend=False)
+        return fig
 
 
 if __name__ == "__main__":
     f = lambda x: -5 * x**5 + 4 * x**4 - 12 * x**3 + 11 * x**2 - 2 * x + 1  # -0.5 0.5
-    task = GoldenRatio(f, (-0.5, 0.5), print_interim=False, save_iters_df=True)
+    task = GoldenRatio(f, (-0.5, 0.5), print_interim=False, save_iters_df=False, draw_flag=True)
     res = task.solve()
-    print(res[1])
+    res[1].show()
